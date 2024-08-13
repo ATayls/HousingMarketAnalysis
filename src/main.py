e366e8a116aa94be2cc308c86b8040931a901565
ATayls <adam.taylor300@yahoo.co.uk>
2024-08-13 20:44:50 +0100

    Add basic plots

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from extract import get_data
-
+from plots import plot_property_scatter, plot_property_prices_over_time
 
 def main():
     search_area = 'Epsom'
@@ -7,6 +7,9 @@ def main():
 
     print(f'Running Extract for {search_area}')
     sold_properties_df = get_data(search_area, limit)
+
+    plot_property_scatter(sold_properties_df)
+    plot_property_prices_over_time(sold_properties_df)
 
     return sold_properties_df
 
